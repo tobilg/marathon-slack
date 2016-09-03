c65f0621c6c55bf53a71c03790fa84b8e0966205
tobilg <tobilg@gmail.com>
2016-09-03 21:59:56 +0200

    Docs and refactoring

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 "use strict";
 
 // Check if we got the necessary info from the environment, otherwise fail directly!
-require("require-environment-variables")(["WEBHOOK_URL"]);
+require("require-environment-variables")(["SLACK_WEBHOOK_URL"]);
 
+// Load event listener
 var MarathonEventListener = require("./lib/MarathonEventListener");
 
+// Define options
 var options = {
     marathonUrl: process.env.MARATHON_URL || "leader.mesos",
     marathonPort: process.env.MARATHON_PORT || 8080,
-    slackWebHook: process.env.WEBHOOK_URL,
+    marathonProtocol: process.env.MARATHON_PROTOCOL || "http",
+    slackWebHook: process.env.SLACK_WEBHOOK_URL,
+    slackChannel: process.env.SLACK_CHANNEL || "#marathon",
     logging: {
         level: process.env.LOG_LEVEL || "info"
     }
@@ -20,14 +24,18 @@ if (process.env.EVENT_TYPES) {
     }
 }
 
+// Create event listener
 var mel = new MarathonEventListener(options);
 
+// Report connection
 mel.on("connected", function (timestamp) {
     mel.logger.info("Connected to the Marathon Event Bus!");
 });
 
+// Report errors
 mel.on("error", function (error) {
     mel.logger.error(JSON.stringify(error));
 });
 
+// Subscribe to Marathon event bus
 mel.subscribe();
